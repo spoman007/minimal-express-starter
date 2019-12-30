51a77fe6a10deebfee14529a0cecdc6f69ede906
DEV-POMANS <DEV-POMANS@iaea.org>
2019-12-30 18:52:52 +0100

    Adding script to copy the node-starter repo

---
+++ index.js
@@ -0,0 +1,47 @@
+#! /usr/bin/env node
+
+const { spawn } = require('child_process');
+
+const name = process.argv[2];
+if (!name || name.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
+  return console.log(`
+  Invalid directory name.
+  Usage: create-express-api name-of-api  
+`);
+}
+
+const repoURL = 'https://github.com/spoman007/node-starter.git';
+
+runCommand('git', ['clone', repoURL, name])
+  .then(() => {
+    return runCommand('rm', ['-rf', `${name}/.git`]);
+  }).then(() => {
+    console.log('Installing dependencies...');
+    return runCommand('npm', ['install'], {
+      cwd: process.cwd() + '/' + name
+    });
+  }).then(() => {
+    console.log('Done! 🏁');
+    console.log('');
+    console.log('To get started:');
+    console.log('cd', name);
+    console.log('npm run dev');
+  });
+
+function runCommand(command, args, options = undefined) {
+  const spawned = spawn(command, args, options);
+
+  return new Promise((resolve) => {
+    spawned.stdout.on('data', (data) => {
+      console.log(data.toString());
+    });
+    
+    spawned.stderr.on('data', (data) => {
+      console.error(data.toString());
+    });
+    
+    spawned.on('close', () => {
+      resolve();
+    });
+  });
+}
